98b31baeb8755a50da9f45014fd73305f132f116
Gabrielle Pantin <pantin.gabrielle@gmail.com>
2021-09-19 17:51:12 -0500

    frontend setup

---
+++ src/components/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {
+  BrowserRouter as Router, 
+  Switch, 
+  Route,
+  } from 'react-router-dom';
+
+
+const App = () => {
+  return (
+    <>
+
+        <Router>
+
+            <Switch>
+            
+                <Route>
+
+
+
+                </Route>
+            
+            </Switch>
+        
+        </Router> 
+    
+    </>
+  )
+}
+
+export default App
